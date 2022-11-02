127c7764f9f0f8525780831e25c42ece1d9c5941
white <white@example.com>
2022-11-02 21:10:36 +0500

    Hello everyone

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -11,7 +11,7 @@ namespace HelloApp
         
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello everyone");
         }
     }
 }
